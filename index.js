73eab136ac662d1994a0eaa8152afdeb7d8d5bc9
Abhimanue <abhimanuems@hotmail.com>
2023-12-06 15:00:07 +0530

    change in pacakage

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 return emailRegex.test(email);
 }
 
+function isNumber(number){
+    return !isNaN(Number(number))
+}
+
+
+
 module.exports = {
   isValidEmail,
+  isNumber,
 };
